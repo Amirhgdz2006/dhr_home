0650559b336007f27abf18954ba1c35e6bbbd95f
Amirhgdz2006 <amirh.goodarzi.2006@gmail.com>
2026-01-02 14:43:24 +0330

    cleaning the code and adding new background

--- apps/backend/src/models/App.js
+++ apps/backend/src/models/App.js
@@ -3,19 +3,19 @@ const Counter = require('./Counter');
 
 const appSchema = new mongoose.Schema({ 
   id: { type: Number },
-    name: { type: String, required: true },
-    englishName: { type: String },
-    description: { type: String },
-    url: { type: String },
-    keywords: [String],
-    icon_background_color: { type: String, default: null },
-    icon: {
-    url: String,
-    },
-    category : {type: String},
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
-    publishedAt: { type: Date, default: Date.now }
+  name: { type: String, required: true },
+  englishName: { type: String },
+  description: { type: String },
+  url: { type: String },
+  keywords: [String],
+  icon_background_color: { type: String, default: null },
+  icon: {
+  url: String,
+  },
+  category : {type: String},
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  publishedAt: { type: Date, default: Date.now }
 });
 
 appSchema.pre('save', async function () {
